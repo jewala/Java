0e214eb22ce19b713f47bcd5f6d3aa9e134e9bcd
Jesper Wagner <j.wagnerlarsson@gmail.com>
2024-05-25 14:39:11 +0200

    cleared up shit

--- base-variables.js
+++
@@ -1,4 +0,0 @@
-let baseUrl = {
-    url: 'http://127.0.0.1:5500/index.html',
-};
-
--- tests/localhost-test.spec.js
+++ tests/localhost-test.spec.js
@@ -1,6 +1,6 @@
 const { test, expect } = require('@playwright/test');
 
-let page = baseUrl;
+let page = "http://127.0.0.1:5500/index.html";
 
 test('title check', async ({ page }) => {
     await page.goto(baseUrl);
