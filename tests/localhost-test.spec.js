7ec5ec3846ec9d59b227b72e0fce3e9fb7d7700c
Jesper Wagner <j.wagnerlarsson@gmail.com>
2024-05-25 15:12:24 +0200

    update

--- tests/localhost-test.spec.js
+++ tests/localhost-test.spec.js
@@ -1,8 +1,7 @@
 const { test, expect } = require('@playwright/test');
 
-let page = "http://127.0.0.1:5500/index.html";
 
 test('title check', async ({ page }) => {
-    await page.goto(page);
+    await page.goto("http://127.0.0.1:5500/index.html");
     await page.expect(page).toHaveTitle("Page title");
 });
