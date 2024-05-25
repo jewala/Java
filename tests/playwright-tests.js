dff2ab8be6a8a8e7f727f6383f4c6749d8846d2b
Jesper Wagner <j.wagnerlarsson@gmail.com>
2024-05-25 15:25:20 +0200

    fed

--- tests/playwright-tests.js
+++ tests/playwright-tests.js
@@ -1,7 +1,8 @@
+
 import { test, expect } from '@playwright/test';
 const { test, expect } = require('@playwright/test');
 
-
+// @ts-check
 test('title check', async ({ page }) => {
     await page.goto("http://127.0.0.1:5500/index.html");
     await expect(page).toHaveTitle("Page title");
