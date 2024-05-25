ad52a7849e6f7517693416f694cf3eae73a4fba2
Jesper Wagner <j.wagnerlarsson@gmail.com>
2024-05-25 15:19:28 +0200

    renamed test file, deleted example test file, added new test for alarm button

--- tests/playwright-tests.js
+++ tests/playwright-tests.js
@@ -5,3 +5,9 @@ test('title check', async ({ page }) => {
     await page.goto("http://127.0.0.1:5500/index.html");
     await expect(page).toHaveTitle("Page title");
 });
+
+test('alarm button check', async ({ page }) => {
+    await page.goto("http://127.0.0.1:5500/index.html");
+    page.getByTestId("test-button").click;
+    page.getByText("OK");
+});
