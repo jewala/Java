38d42754a1b1ef56e49750560f7471835333db59
Jesper Wagner <j.wagnerlarsson@gmail.com>
2024-05-25 14:40:35 +0200

    updated base url to page variable

--- tests/localhost-test.spec.js
+++ tests/localhost-test.spec.js
@@ -3,6 +3,6 @@ const { test, expect } = require('@playwright/test');
 let page = "http://127.0.0.1:5500/index.html";
 
 test('title check', async ({ page }) => {
-    await page.goto(baseUrl);
-    await page.expect(baseUrl).toHaveTitle("Page title");
+    await page.goto(page);
+    await page.expect(page).toHaveTitle("Page title");
 });
