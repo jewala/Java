839ffe334990e330badffb156d9d22d616b3f2a2
Jesper Wagner <j.wagnerlarsson@gmail.com>
2024-05-29 15:41:46 +0200

    tests

--- tests/page-scan.spec.js
+++ tests/page-scan.spec.js
@@ -2,7 +2,6 @@ import { test, expect } from '@playwright/test';
 
 test('test', async ({ page }) => {
   await page.goto('http://127.0.0.1:5500/index.html');
-  expect(page.title).toContain('Page title');
   await expect(page.locator('#page-heading')).toContainText('This is a heading');
   await expect(page.locator('#test-paragraph')).toContainText('This is a paragraph.');
   await expect(page.locator('#test-button')).toContainText('This is an alert button.');
