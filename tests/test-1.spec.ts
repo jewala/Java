de646baede89c283551bd0f26b735d81e6201b22
Jesper Wagner <j.wagnerlarsson@gmail.com>
2024-05-29 15:39:14 +0200

    tests

--- tests/test-1.spec.ts
+++
@@ -1,6 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('test', async ({ page }) => {
-  await page.goto('about:blank');
-  await page.goto('chrome-error://chromewebdata/');
-});
--- tests/test-2.spec.ts
+++
@@ -1,8 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('page-scan', async ({ page }) => {
-  await page.goto('http://127.0.0.1:5500/');
-  await expect(page.locator('#page-heading')).toContainText('This is a heading');
-  await expect(page.locator('#test-paragraph')).toContainText('This is a paragraph.');
-  await expect(page.locator('#test-button')).toContainText('This is an alert button.');
-});
