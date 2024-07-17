3d451d5ccffec498e32ea9f5c72d13463958a0eb
Jesper Wagner <j.wagnerlarsson@gmail.com>
2024-07-17 12:17:56 +0200

    Updated tests and input fields

--- tests/page-scan.spec.js
+++ tests/page-scan.spec.js
@@ -2,8 +2,15 @@ import { test, expect } from '@playwright/test';
 
 test('Heading, paragraph, button', async ({ page }) => {
   await page.goto('http://127.0.0.1:5500/index.html');
-  await expect(page.locator('#page-heading')).toContainText('This is a heading');
-  await expect(page.locator('#test-paragraph')).toContainText('This is a paragraph.');
-  await expect(page.locator('#test-button')).toContainText('This is an alert button.');
+  await expect(page.locator('#page-heading')).toContainText('About frogs');
+  await expect(page.locator('#alert-button')).toContainText('This is an alert button.');
   await page.getByRole('button', { name: 'This is an alert button.' }).click();
 });
+
+test('input fields, alert button', async ({ page }) => {
+  await expect(page.locator('#fname')).toBeEmpty;
+  await expect(page.locator('#lname')).toBeEmpty;
+  await expect(page.locator('#submit-button')).toBeVisible;
+  await page.getByRole('button', { name: 'Submit information'}).click();
+});
+
